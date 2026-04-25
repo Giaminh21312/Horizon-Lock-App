6741762d6678c15229a8ae23aa0a4879de263704
ImMinhh <nguyen1122134lamgiaminh@gmail.com>
2026-04-25 10:13:49 +0000

    Fix can't make file

--- ProjectConfig.rb
+++ ProjectConfig.rb
@@ -1,10 +1,24 @@
 require 'xcodeproj'
+
 project_path = 'HorizonLockApp.xcodeproj'
 project = Xcodeproj::Project.new(project_path)
+
+# Tạo target cho iOS 15
 target = project.new_target(:application, 'HorizonLockApp', :ios, '15.0')
-target.add_file_references(Dir.glob('HorizonLockApp/*.swift'))
+
+# Sửa lỗi ở đây: Tạo reference cho file trước khi add vào target
+file_path = 'HorizonLockApp/main.swift'
+file_ref = project.main_group.new_file(file_path)
+target.add_file_references([file_ref])
+
+# Cấu hình Build Settings
 target.build_configurations.each do |config|
   config.build_settings['INFOPLIST_FILE'] = 'HorizonLockApp/Info.plist'
   config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.0'
+  config.build_settings['SWIFT_VERSION'] = '5.0'
+  config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
+  config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
 end
+
 project.save
+puts "Project Xcode đã được tạo thành công rồi nhé ông!"
